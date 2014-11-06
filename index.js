4c0be9156ae1badbbb59f7382d9757a581df06b6
wilsaj <wilson.andrew.j+github@gmail.com>
2014-11-06 14:41:40 -0600

    return a clean warning object

--- index.js
+++ index.js
@@ -2,6 +2,45 @@
 var FeedParser = require('feedparser');
 
 
+function nestedProperty(obj, keys) {
+  if (obj === undefined || keys.length === 0) {
+    return obj;
+  }
+  return nestedProperty(obj[keys[0]], keys.slice(1));
+}
+
+function parseItem(item) {
+  var obj = {
+    url: item.link
+  };
+
+  var properties = [
+    'atom:published',
+    'atom:summary',
+    'atom:title',
+    'atom:updated',
+    'cap:areaDesc',
+    'cap:category',
+    'cap:certainty',
+    'cap:effective',
+    'cap:event',
+    'cap:expires',
+    'cap:msgType',
+    'cap:severity',
+    'cap:status',
+    'cap:urgency'
+  ];
+
+  properties.forEach(function (property) {
+    var type = property.split(':')[0];
+    var name = property.split(':')[1];
+    var feedProperty = type + ':' + name.toLowerCase();
+    obj[name] = nestedProperty(item, [feedProperty, '#']);
+  });
+
+  return obj;
+}
+
 module.exports = {
   parse: function (readableStream, cb) {
     var alerts = [];
@@ -22,7 +61,8 @@ module.exports = {
           item;
 
       while (item = stream.read()) {
-        alerts.push(item);
+        var obj = parseItem(item);
+        alerts.push(obj);
       }
     });
 
--- test/index.js
+++ test/index.js
@@ -8,11 +8,96 @@ var parser = require('../index.js');
 test('count matches', function (t) {
   t.plan(1);
 
-  var filepath = path.join(__dirname, 'files/example.xml');
+  var filepath = path.join(__dirname, 'files/multiple.xml');
   var readable = fs.createReadStream(filepath);
 
   parser.parse(readable, function (error, parsed) {
     t.equals(parsed.length, 39);
+  });
+});
+
+
+test('parse alert', function (t) {
+  var filepath = path.join(__dirname, 'files/single.xml');
+  var readable = fs.createReadStream(filepath);
+
+  parser.parse(readable, function (error, parsed) {
+    var single = parsed[0];
+
+    t.test('areaDesc is correctly assigned', function (t2) {
+      t2.plan(1);
+      t2.equals(single.areaDesc, 'Cape Decision to Salisbury Sound Coastal Area; Inner Channels from Kupreanof Island to Etolin Island');
+    });
+
+    t.test('category is correctly assigned', function (t2) {
+      t2.plan(1);
+      t2.equals(single.category, 'Met');
+    });
+
+    t.test('certainty is correctly assigned', function (t2) {
+      t2.plan(1);
+      t2.equals(single.certainty, 'Likely');
+    });
+
+    t.test('effective is correctly assigned', function (t2) {
+      t2.plan(1);
+      t2.equals(single.effective, '2014-11-05T05:25:00-09:00');
+    });
+
+    t.test('event is correctly assigned', function (t2) {
+      t2.plan(1);
+      t2.equals(single.event, 'High Wind Warning');
+    });
+
+    t.test('expires is correctly assigned', function (t2) {
+      t2.plan(1);
+      t2.equals(single.expires, '2014-11-06T00:00:00-09:00');
+    });
+
+    t.test('msgType is correctly assigned', function (t2) {
+      t2.plan(1);
+      t2.equals(single.msgType, 'Alert');
+    });
+
+    t.test('published is correctly assigned', function (t2) {
+      t2.plan(1);
+      t2.equals(single.published, '2014-11-05T05:25:00-09:00');
+    });
+
+    t.test('severity is correctly assigned', function (t2) {
+      t2.plan(1);
+      t2.equals(single.severity, 'Severe');
+    });
+
+    t.test('summary is correctly assigned', function (t2) {
+      t2.plan(1);
+      t2.equals(single.summary, '...HIGH WIND WARNING REMAINS IN EFFECT FROM 3 PM THIS AFTERNOON TO MIDNIGHT AKST TONIGHT... * LOCATION...PORTIONS OF THE THE SOUTHERN PANHANDLE INCLUDING SOUTHERN BARANOF ISLAND...KUIU ISLAND...SOUTHERN KUPREANOF ISLAND...ETOLIN ISLAND AND ZAREMBO ISLAND. * WIND...25 TO 40 MPH WITH GUSTS TO AROUND 60 MPH. STRONGEST WINDS IN');
+    });
+
+    t.test('status is correctly assigned', function (t2) {
+      t2.plan(1);
+      t2.equals(single.status, 'Actual');
+    });
+
+    t.test('title is correctly assigned', function (t2) {
+      t2.plan(1);
+      t2.equals(single.title, 'High Wind Warning issued November 05 at 5:25AM AKST until November 06 at 12:00AM AKST by NWS');
+    });
+
+    t.test('updated is correctly assigned', function (t2) {
+      t2.plan(1);
+      t2.equals(single.updated, '2014-11-05T06:25:00-09:00');
+    });
+
+    t.test('urgency is correctly assigned', function (t2) {
+      t2.plan(1);
+      t2.equals(single.urgency, 'Expected');
+    });
+
+    t.test('url is correctly assigned', function (t2) {
+      t2.plan(1);
+      t2.equals(single.url, 'http://alerts.weather.gov/cap/wwacapget.php?x=AK1251776ADAE4.HighWindWarning.125177795010AK.AJKNPWAJK.0b2fbe9c62ef103901a2841f43c56168');
+    });
   });
 });
 
