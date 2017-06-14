b37650163708a653e3790fef97850c162a5a559b
wilsaj <wilson.andrew.j+github@gmail.com>
2017-06-14 12:01:17 -0500

    Fix example code by including a `User-Agent:` header

--- README.md
+++ README.md
@@ -18,7 +18,14 @@ var fs = require('fs');
 var parser = require('weather-alerts-parser');
 var request = require('request');
 
-request.get('http://alerts.weather.gov/cap/us.php?x=1')
+request.get({
+  url: 'https://alerts.weather.gov/cap/us.php?x=1',
+  headers: {
+    // The request will fail with a 403 Forbidden status code if User-Agent is
+    // empty, so we need to set one. Anything works.
+    'User-Agent': 'lol',
+  }
+})
   .pipe(parser.stream())
   .pipe(es.stringify())
   .pipe(process.stdout);
--- examples/current-alerts.js
+++ examples/current-alerts.js
@@ -4,7 +4,14 @@ var request = require('request');
 var es = require('event-stream');
 var parser = require('../lib/index.js');
 
-request.get('http://alerts.weather.gov/cap/us.php?x=1')
+request.get({
+  url: 'https://alerts.weather.gov/cap/us.php?x=1',
+  headers: {
+    // The request will fail with a 403 Forbidden status code if User-Agent is
+    // empty, so we need to set one. Anything works.
+    'User-Agent': 'lol',
+  }
+})
   .pipe(parser.stream())
   .pipe(es.stringify())
   .pipe(process.stdout);
